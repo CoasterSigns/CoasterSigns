12c32bcfaefca67ecc3501cf2675d01edad88667
masp005 <spelsberg.marcel@gmail.com>
2024-06-11 01:32:55 +0200

    TimedScriptExecutor build message

--- src/main/java/dev/masp005/coastersigns/signs/SignActionTimedScript.java
+++ src/main/java/dev/masp005/coastersigns/signs/SignActionTimedScript.java
@@ -4,6 +4,7 @@ import com.bergerkiller.bukkit.tc.events.SignActionEvent;
 import com.bergerkiller.bukkit.tc.events.SignChangeActionEvent;
 import com.bergerkiller.bukkit.tc.signactions.SignAction;
 import com.bergerkiller.bukkit.tc.signactions.SignActionType;
+import com.bergerkiller.bukkit.tc.utils.SignBuildOptions;
 import de.themoep.timedscripts.TimedScripts;
 import dev.masp005.coastersigns.CoasterSigns;
 import dev.masp005.coastersigns.Util;
@@ -47,7 +48,12 @@ public class SignActionTimedScript extends CSBaseSignAction {
             pl.logWarn("Script " + info.getLine(2) + " not found! " + Util.blockCoordinates(info.getBlock()), debugName + ".execution");
     }
 
-    public boolean build(SignChangeActionEvent signChangeActionEvent) {
+    public boolean build(SignChangeActionEvent info) {
+        SignBuildOptions message = SignBuildOptions.create()
+                .setHelpURL(helpLink)
+                .setName("TimedScript Executor")
+                .setDescription(ready ? basicDesc : basicDesc + ".\n\n§cError: TimedScripts is not installed, so this sign will not work.");
+        message.handle(info.getPlayer());
         return ready;
     }
 
